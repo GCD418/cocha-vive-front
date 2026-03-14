e782336f791b81a317a5d1cd613aa5d7e73740cb
Diego Rios <diegoarv08@hotmail.com>
2026-03-14 11:50:19 -0400

    fix(category.service): Se estaba llamando a otra direccion de la API

--- src/app/services/category.service.ts
+++ src/app/services/category.service.ts
@@ -8,7 +8,7 @@ import { Category } from '../models/category.model';
 })
 export class CategoryService {
 
-  private apiUrl = 'http://localhost:8080/api/v1/categories';
+  private apiUrl = 'http://localhost:8080/api/categories';
 
   constructor(private http: HttpClient) {}
 
