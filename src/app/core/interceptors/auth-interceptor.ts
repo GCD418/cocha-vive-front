4eaf0f417ff670344f618129172ab9f76f5744bc
gabriel <patosquare418@gmail.com>
2026-03-22 22:16:57 -0400

    fix: avoiding token injection in api/auth routes

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,6 +1,10 @@
 import { HttpInterceptorFn } from '@angular/common/http';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  if (req.url.includes('/api/auth/')) {
+    return next(req);
+  }
   const token = localStorage.getItem('cocha_vive_token');
 
   if (token) {
