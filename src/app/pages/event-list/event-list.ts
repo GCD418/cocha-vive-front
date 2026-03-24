c5ab1031059439c2d81811a48ec5e1a4e0abd9e1
christian-ledezma <chris.ledezma.s@gmail.com>
2026-03-24 01:14:25 -0400

    Feat: Add edit modal

--- src/app/pages/event-list/event-list.ts
+++ src/app/pages/event-list/event-list.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { EventService } from '../../services/event-service/event.service';
 import { Router, RouterLink } from '@angular/router';
 import { EventModel } from '../../models/event-model';
 import { PricePipe } from '../../shared/pipes/price.pipe';
+import { EventFormResult, EventFormComponent } from '../../components/events/event-form-component/event-form-component';
+
+declare const bootstrap: any;
 
 @Component({
   selector: 'app-event-list',
+  standalone: true,
   imports: [CommonModule, RouterLink, PricePipe],
   templateUrl: './event-list.html',
   styleUrl: './event-list.css',
@@ -14,6 +18,9 @@ import { PricePipe } from '../../shared/pipes/price.pipe';
 export class EventList {
 
   events: EventModel[] = [];
+  selectedEvent: EventModel | null = null;
+
+   private editModal: any;
 
   constructor(private eventService : EventService,
               private router: Router) { }
@@ -31,4 +38,27 @@ export class EventList {
   eventDetails(id: number){
     this.router.navigate(['/event-details', id]);
   }
+
+  openEditModal(event: EventModel): void {
+    this.selectedEvent = event;
+
+    const modalEl = document.getElementById('editEventModal');
+    if (modalEl) {
+      this.editModal = new bootstrap.Modal(modalEl);
+      this.editModal.show();
+    }
+  }
+
+   onEditFormResult(result: EventFormResult): void {
+    if (this.editModal) {
+      this.editModal.hide();
+    }
+    this.selectedEvent = null;
+
+    if (result.success) {
+      this.getEvents();
+    }
+  }
+
+
 }
