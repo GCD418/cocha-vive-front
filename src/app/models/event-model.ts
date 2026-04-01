066d95c2d1b66e96f267112d60526df94c51d174
christian-ledezma <chris.ledezma.s@gmail.com>
2026-04-01 09:39:37 -0400

    Fix: eventStatus type

--- src/app/models/event-model.ts
+++ src/app/models/event-model.ts
@@ -24,7 +24,7 @@ export interface EventModel {
     shortPlaceDescription: string;
     tags: string[];
     photoLinks: string[];
-    eventStatus: boolean;
+    eventStatus: string;
     reviewedByAdminId?: number;
     createdAt: string;
     updatedAt?: string;
