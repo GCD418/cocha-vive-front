ac4fa3540f1165b0f677e4ed74b650139001cc49
gabriel <patosquare418@gmail.com>
2026-04-05 20:13:27 -0400

    feat: deleting Jwt when backend responds forbidden

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,20 +1,35 @@
-import { HttpInterceptorFn } from '@angular/common/http';
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from '../../services/auth/auth.service';
+import { inject } from '@angular/core';
+import { Router } from '@angular/router';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
+
+  const authService = inject(AuthService);
+  const router = inject(Router);
 
   if (req.url.includes('/api/auth/')) {
     return next(req);
   }
-  const token = localStorage.getItem('cocha_vive_token');
+  const token = authService.getToken();
+  let petition = req;
 
   if (token) {
-    const clonedRequest = req.clone({
+    petition = req.clone({
       setHeaders: {
         Authorization: `Bearer ${token}`
       }
     });
-    return next(clonedRequest);
   }
 
-  return next(req);
+ return next(petition).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401 || error.status === 403) {
+        authService.logout();
+        router.navigate(['/login']);
+      }
+      return throwError(() => error);
+    })
+  ); 
 };
