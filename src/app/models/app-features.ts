74b0d9bef7f81483b195b0c3f427c070f2c0a7a3
GCD418 <patosquare418@gmail.com>
2026-06-18 17:55:51 -0400

    [CV-126] deleted final feature toggle and unnecessary imports (#80)
    # Summary
    Feature flag for `MANAGE_PUBLISHER_REQUESTS` has been deleted as now is
    an always active function.
    Also, unnecessary imports related to feature flag service has been
    erased from this dimension.
    
    # Tests
    All tests are currently passing
    <img width="1066" height="311" alt="image"
    src="https://github.com/user-attachments/assets/66b33733-65d8-463a-951b-618120551341"
    />
    
    Co-authored-by: Gabriel <gabriel@cochavive.com>

--- src/app/models/app-features.ts
+++ src/app/models/app-features.ts
@@ -1,6 +1,4 @@
-export const AppFeatures = {
-    MANAGE_PUBLISHER_REQUESTS: 'manage_publisher_requests',
-} as const;
+export const AppFeatures = {} as const;
 
 export type AppFeatureKey = typeof AppFeatures[keyof typeof AppFeatures];
 
